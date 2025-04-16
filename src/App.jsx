41fc6ae66da320595ebf8a94eb2b8417e01e144e
webQbe <webcubesolutionslk@gmail.com>
2025-04-16 20:08:26 +0530

    Displayed SpaceX logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import logo from './logo.png' // Import logo image
 
 function App() {
   return (
     <div className="App">
-      <h1>SpaceX</h1>
+      {/* Show SpaceX logo */}
+      <img 
+        src={logo} 
+        alt="SpaceX"
+        style={{ width: 300, display: 'block', margin: 'auto' }} 
+      />
     </div>
   )
 }
