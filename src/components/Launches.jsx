5e89520cfd6abd9b6dbaa41f292a49fdad1c5d57
webQbe <webcubesolutionslk@gmail.com>
2025-04-16 22:03:48 +0530

    Used Apollo Client's useQuery hook

--- src/components/Launches.jsx
+++ src/components/Launches.jsx
@@ -18,9 +18,29 @@ const LAUNCHES_QUERY = gql`
 `;
 
 const Launches = () => {
+
+    // Run GraphQL query
+    const { 
+            loading, // 'true' while the request is in flight
+            error,   // An 'Error' object if the query fails
+            data     // The actual query result (your launches data)
+        } = useQuery(LAUNCHES_QUERY);
+
+    // While waiting for data, show a loading message.
+    if (loading) return <h4>Loading...</h4>; 
+    // If the query fails, log the error to the console
+    if (error) console.log(error); 
+    // When data is ready, log the data object                   
+    console.log(data)    
+
+    const test = 'test'; // a regular string   
+
   return (
     <div>
         <h1 className="display-4 my-3">Launches</h1>
+        
+        <h1>{ test }</h1> 
+        {/* Everything up to this point is working (good for debugging). */}
     </div>
   )
 }
