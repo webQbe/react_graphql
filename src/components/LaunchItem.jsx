edfe7c72dbdfdc2c3ddca9594622d3510f20ebe7
webQbe <webcubesolutionslk@gmail.com>
2025-04-17 12:50:09 +0530

    Destructured parameters, created Bootstrap card+layout, & added button placeholder for each individual launch

--- src/components/LaunchItem.jsx
+++ src/components/LaunchItem.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 
-const LaunchItem = ({ launch }) => { // Destructure {launch} prop passed from Launches
-  console.log(launch);               // To inspect each launch object 
-  return (
-    <div>test</div> /* return a placeholder */
-  )
+const LaunchItem = ({ // Destructure {launch} prop passed from Launches
+                      launch : { flight_number,
+                                 mission_name,
+                                 launch_date_local,
+                                 launch_success }
+                    }) => { 
+    return (
+            <div className="card card-body mb-3"> 
+            {/* Bootstrap card to neatly group content */}
+                <div className="row">
+                    {/* A 2-column Bootstrap layout */}
+                    <div className="col-md-9">
+                        {/* Left - Shows mission name and date */}
+                        <h4>Mission: { mission_name }</h4>
+                        <p>Date: { launch_date_local }</p>
+                    </div>
+                    <div className="col-md-3">
+                        {/* Right - Bootstrap-styled button */}
+                        <button className="btn btn-secondary">
+                            Launch Details
+                        </button>
+                    </div>
+                </div>
+
+            </div>
+    )
 }
 
 export default LaunchItem
